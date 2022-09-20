5d711234382ca314788c5b7d9c670a856ca0173e
MarkAlexanderBennett <alex220mark@gmail.com>
2022-09-20 13:02:34 +1000

    remove TODOs

--- src/main/java/rpggame/RpgGameApp.java
+++ src/main/java/rpggame/RpgGameApp.java
@@ -8,9 +8,7 @@ public class RpgGameApp {
 
 	public static void main(String[] args) {
 
-		// SAVE GAME TO A TXT FILE WITH STATS, and LOAD IN A GAME with story progression
 		// tests
-		// add sound?
 		// package into a jar/exe file for execution on any computer
 		// TODO: Combat Interface and have CombatCrits and CombatNoCrits, have a
 		// CombatService for all the doubled methods?
